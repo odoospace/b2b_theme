66b61113f6fe2cc4a4f61166a6863d021d4c343f
Pedro Gracia <pedro.gracia@impulzia.com>
2020-10-27 11:56:48 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    # Theme information
+    'name': "B2B Theme",
+    'description': """
+    """,
+    'category': 'Theme/Ecommerce',
+    'version': '12.0.1.6.0',
+    'depends': ['website','website_theme_install'],
+
+    # templates
+    'data': [
+    #    'views/layout.xml',
+        'views/pages.xml',
+        'views/templates.xml',
+        'views/assets.xml'
+    #     'views/options.xml',
+    #    'views/snippets.xml',
+    ],
+
+    # demo pages
+    'demo': [
+        'demo/pages.xml',
+    ],
+
+    # Your information
+    'author': "Impulso Diagonal S.L.U.",
+    'website': "",
+}
