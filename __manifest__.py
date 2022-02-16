44930a0c761b4f3484a716cede41e7a353ac40c9
alfonsomb <alfonso@impulso.xyz>
2022-02-16 17:47:20 +0100

    Module migrated to 14 version

--- __manifest__.py
+++ __manifest__.py
@@ -4,24 +4,14 @@
     'description': """B2B Theme
     """,
     'category': 'Theme',
-    'version': '12.0.2.0.1',
+    'version': '14.0.2.0.1',
     'depends': ['b2b_base'],
 
     # templates
     'data': [
-    #    'views/layout.xml',
-        # 'views/pages.xml',
         'views/templates.xml',
         'views/assets.xml',
-        # 'views/website_sale.xml',
-    #     'views/options.xml',
-    #    'views/snippets.xml',
     ],
-
-    # demo pages
-    #'demo': [
-    #    'demo/pages.xml',
-    #],
 
     # Your information
     'author': "Impulso Diagonal S.L.U.",
